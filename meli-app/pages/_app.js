26ec76c597caf045698debf5a45477ab4681c800
Walter <walgom4@gmail.com>
2021-09-02 03:20:32 -0500

    feat: adding i18n, adjusting project to use node

---
+++ meli-app/pages/_app.js
@@ -0,0 +1,8 @@
+import '../styles/globals.css'//global styles
+import '../i18n'//internationalization library
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
